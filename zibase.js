b2cb4df443f94c06fd86b8c627b60eaa46b1964f
Baptiste Marienval <baptiste.marienval@gmail.com>
2013-06-14 09:27:35 +0200

    v 2.1 = Suppression attribut Type

--- zibase.js
+++ zibase.js
@@ -4,6 +4,8 @@ exports.action = function(data, callback, config){
 	var url="";
 	var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 	var actionList = ['OFF', 'ON', 'DIM', 'LM'];
+	
+	var actionType;
 		
 	/***** LAN method *****/
 	if(config.acces_method == "lan") {
@@ -13,8 +15,8 @@ exports.action = function(data, callback, config){
 		}
 	url = config.api_url_lan;
 	
-	if (data.actionType == 0) {
-		//periph
+	if (data.actionAdd2) {
+		// une adresse de module = actin sur periph
 		url += '?cmd='+data.actionModule+'%20'+data.actionAdd2+data.actionAdd1+'%20P'+data.actionProto;
 		}
 	else {
@@ -32,27 +34,29 @@ exports.action = function(data, callback, config){
 		}
 		url = config.api_url_web;
    
-		// si type = 0 (action sur un peripherique)
-		if (data.actionType == 0) {
+		// si add existe = action sur un peripherique
+		if (data.actionAdd2) {
 			// Conversion format action/proto
 			var param2convert = "";
 			param2convert = FormatNumberLength(parseInt(data.actionProto,10).toString(2),8);
 			param2convert += FormatNumberLength(actionList.indexOf(data.actionModule), 8);
 			// Conversion de la lettre du module
 			var param4convert = "";
-			param4convert = aphabet.indexOf(data.actionAdd2);
+			param4convert = alphabet.indexOf(data.actionAdd2);
+			actionType=0;
 		}
-		// action sur scenario
+		// si pas d'add = action sur scenario
 		else {
 			param2convert=parseInt(data.actionScenario, 10).toString(2);
+			actionType=1;
 		}
   
 		url += '?device='+config.device;
 		url += '&token='+config.token;
 		url += '&action=rowzibasecommand';
-		url += '&param1='+data.actionType; // 0 = emission RF, 1= scenario
+		url += '&param1='+actionType; // 0 = emission RF, 1= scenario
 		url += '&param2='+parseInt(param2convert,2).toString(10); // 0..7 = action (0=off, 1=on) ; 8..15 proto (0=chacon, 6=zwave)
-		if (data.actionType == 0) {
+		if (actionType == 0) {
 			url += '&param3='+(parseInt(data.actionAdd1)-1); // chiffre adresse-1
 			url += '&param4='+param4convert; // lettre adresse A=0, B=1 ...
 		}
