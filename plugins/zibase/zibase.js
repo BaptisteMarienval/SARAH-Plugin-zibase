198c23d52e94e4ee110292aa3a1fcdc20ab86451
Baptiste Marienval <baptiste.marienval@gmail.com>
2013-07-02 19:57:46 +0200

    2.3

--- plugins/zibase/zibase.js
+++ plugins/zibase/zibase.js
@@ -18,17 +18,11 @@ exports.action = function(data, callback, config){
 **************************************************/	
 
 	/** acces module **/
-	console.log(data.module);
 	var fs = require('fs');
-
-	var xmlFile2 = fs.readFileSync(__dirname+'\\periph.xml');
-
+	var xmlFile = fs.readFileSync(__dirname+'\\periph.xml');
 	var file = new xmldoc.XmlDocument(xmlFile);
 	var module = file.childWithAttribute('nom',data.module);
 
-	
-	callback({'tts': module.nom});
-}
 	/***** LAN method *****/
 	if(config.acces_method == "lan") {
 		if (!config.ip_lan){
@@ -36,26 +30,26 @@ exports.action = function(data, callback, config){
 			return;
 		}
 		// periph action
-		if (module.type='module') {
+		if (module.attr.type=='module') {
 			url = http+config.ip_lan+lan_path;
 			// Dimmable action
 			if (data.dimValue){
-				url += '?cmd=DIM%20'+module.addr2+module.addr+'%20P'+module.proto;
+				url += '?cmd=DIM%20'+module.attr.addr+module.attr.addr2+'%20P'+module.attr.proto;
 				url += '%20'+data.dimValue;
 			}
 			// ON/OFF action
 			else { 
-				url += '?cmd='+data.actionModule+'%20'+module.addr2+module.addr1+'%20P'+module.proto;
+				url += '?cmd='+data.actionModule+'%20'+module.attr.addr+module.attr.addr2+'%20P'+module.attr.proto;
 			}
 		}
 		// Sensor action
-		else if (module.type='sonde'){
+		else if (module.attr.type=='sonde'){
 			url = http+config.ip_lan+sensors_path;
 		}
 		// Scenario action
 		else {	
 			url = http+config.ip_lan+lan_path;
-			url += '?cmd=LM%20'+module.addr1;
+			url += '?cmd=LM%20'+module.attr.addr;
 		}
 		console.log("Sending request to: " + url);
 	}
@@ -69,7 +63,7 @@ exports.action = function(data, callback, config){
 		url = https+config.plateforme_web+web_path;
    
 		// periph action
-		if (module.type='module') {
+		if (module.attr.type='module') {
 			// Conversion format action/proto/DimValue => param 2
 			var param2convert = "";
 			
@@ -77,13 +71,13 @@ exports.action = function(data, callback, config){
 				// DIM/BRIGHT
 				param2convert += FormatNumberLength(parseInt(data.dimValue,10).toString(2),8);
 				// PROTOCOL
-				param2convert += FormatNumberLength(parseInt(module.proto,10).toString(2),8);
+				param2convert += FormatNumberLength(parseInt(module.attr.proto,10).toString(2),8);
 				// ACTION
 				param2convert += FormatNumberLength(parseInt(actionList.indexOf('DIM'),10).toString(2), 8);
 			}
 			else {
 				// PROTOCOL
-				param2convert += FormatNumberLength(parseInt(module.proto,10).toString(2),8);
+				param2convert += FormatNumberLength(parseInt(module.attr.proto,10).toString(2),8);
 				// ACTION
 				param2convert += FormatNumberLength(actionList.indexOf(data.actionModule), 8);
 			}
@@ -92,22 +86,22 @@ exports.action = function(data, callback, config){
 			
 			// Conversion du chiffre du module => param 3
 			var param3convert = "";
-			param3convert = parseInt(module.addr2)-1;
+			param3convert = parseInt(module.attr.addr2)-1;
 			
 			// Conversion de la lettre du module => param 4
 			var param4convert = "";
-			param4convert = alphabet.indexOf(module.addr);
+			param4convert = alphabet.indexOf(module.attr.addr);
 			actionType=0;
 		}
 		
-		if (module.type='sonde') {
+		if (module.attr.type='sonde') {
 		callback({'tts': "La gestion des sondes en mode web n'est pour le moment pas possible"});
 			return;
 		}
 		
 		// Scenario action
 		else {
-			param2convert=parseInt(module.addr, 10).toString(2);
+			param2convert=parseInt(module.attr.addr, 10).toString(2);
 			actionType=1;
 		}
 		url += '?device='+config.device;
@@ -141,25 +135,26 @@ exports.action = function(data, callback, config){
 	
 	// Sensors action => Parsing XML
 	// !! les variables v1, v2 peuvent varier selon le péripherique
-	if ((module.type == 'SONDE') && (config.acces_method == "lan")) {
+	if (module.attr.type == 'sonde') {
 		var results = new xmldoc.XmlDocument(body);
 		var evs = results.childNamed("evs");
-		var ev = evs.childWithAttribute('id',module.addr);
+		var ev = evs.childWithAttribute('id',module.attr.addr);
 		var ttsEnd = "";
-		if (module.info == 'temp') {
+
+		if (module.attr.info == 'temp') {
 			var sonde = ev.attr.v1;
-			ttsEnd = 'de ' + parseFloat(sonde/10).toString().replace(".",",") + " degrès";
+			ttsEnd = parseFloat(sonde/10).toString().replace(".",",") + " degrès";
 		}
-		if (module.info == 'hygro') {
+		if (module.attr.info == 'hygro') {
 			var sonde = ev.attr.v2;
-			ttsEnd =  'de ' + parseFloat(sonde).toString().replace(".",",")+" pourcent";
+			ttsEnd =  parseFloat(sonde).toString().replace(".",",")+" pourcent";
 		}
-		if (module.info == 'elec') {
+		if (module.attr.info == 'elec') {
 			var sonde = ev.attr.v2;
-			ttsEnd = 'de ' + parseFloat(sonde*100).toString().replace(".",",")+" kilo watt heure";
+			ttsEnd = parseFloat(sonde*100).toString().replace(".",",")+" kilo watt heure";
 		}
 		
-		if (module.info == 'battery') {
+		if (module.attr.info == 'battery') {
 			var sonde = ev.attr.lowbatt;
 			if (parseFloat(sonde) == 0) {
 			ttsEnd = " bon";
@@ -169,12 +164,12 @@ exports.action = function(data, callback, config){
 			}
 		}
 		
-		tts = module.tts + ttsEnd;
+		tts = module.attr.tts + ttsEnd;
 		
 	}
 	
     else {
-		var tts = data.ttsAction + " " + module.tts;
+		var tts = data.ttsAction + " " + module.attr.tts;
 		if (data.ttsDim){
 			tts += " " + data.ttsDim;
 		}
